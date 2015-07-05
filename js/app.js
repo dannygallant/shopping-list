d053462ad902cf8cf46236be7120ea58402ee80f
dannygallant <danny.gallant@att.net>
2015-07-05 14:30:27 -0500

    Interactive features & images
    Added interaction with jQuery and background image.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-// use enter to add list items
+// allow enter key to add items to the list, in addition to 'add' button
     $('#item').keyup(function(event){
         if(event.keyCode == 13) {
             event.preventDefault();
@@ -9,29 +9,29 @@ $(document).ready(function(){
     }); 
 
 
-
-
-//add list items
+// add items to the list
     $('#addItem').click(function(){
-        var txtbox = document.getElementById('item');
-        var txtval = txtbox.value;
+        var inputField = document.getElementById('item');
+        var value = inputField.value;
         event.preventDefault();
 
         if(!$.trim($('#item').val())) {
-            alert('Please enter text to add to the list');
+            alert('Cannot enter a blank line to the list. Please enter an item.');
         } else {
-            $('<li class="items"></li>').appendTo('#list').html('<input id="checkbox1" type="checkbox"> ' + txtval + '</span><img class="delete" src="img/Trash_icon.png"/>');
+            $('<li class="items"></li>').appendTo('#list').html('<input id="checkbox1" type="checkbox"> ' + value + '</span><img class="delete" src="img/Trash_icon.png"/>');
 
         document.getElementById('item').value = '';
         };
     });
 
-//delete list items
-    $('#list').on('click', '.delete', function(e){e.preventDefault(); $(this).parent().remove()});
+// delete items from the list
+    $('#list').on('click', '.delete', function(e){e.preventDefault(); 
+    $(this).parent().remove()});
 
-//cross off list items
-    $('#list').on('click', 'li', function(){$(this).toggleClass('strike'); $(this).children('.box').toggleClass('Checked');});
+// cross items off the list
+//     $('#list').on('click', 'li.checkbox1', function(){$(this).toggleClass('strikeThrough'); 
+//     $(this).children('.box').toggleClass('checked');});
 
-});
+ });
 
 
